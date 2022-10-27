54567b65f94bccffcfa236a4bde0b4775ee36a26
Jonas Holbech <jh@jonasholbech.dk>
2022-10-27 14:41:55 +0200

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# TV Shows, the form
+
+Our little form playground. Bootstrapped with Vite
+
+If you clone it:
+
+`npm install`
+`npm run dev`
--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import { rangeColorChanger, autoExpandTextarea } from "./utils.js";
 
 rangeColorChanger();
 autoExpandTextarea();
+
+const form = document.querySelector("form");
+
+form.elements.name.addEventListener("input", () => {
+  form.elements.director.value = form.elements.name.value;
+});
+//.length
