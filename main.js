2990c263bd64eadc887ef9d8a522a09395757744
Jonas Holbech <jh@jonasholbech.dk>
2022-10-27 15:49:40 +0200

    posting is working

--- main.js
+++ main.js
@@ -3,19 +3,44 @@ import { rangeColorChanger, autoExpandTextarea } from "./utils.js";
 rangeColorChanger();
 autoExpandTextarea();
 
+const url = "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/tvshows";
+const headers = {
+  apikey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA",
+  "Content-Type": "application/json",
+  Prefer: "return=representation",
+};
+
+function addShow(newShow) {
+  const options = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(newShow),
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
 const form = document.querySelector("form");
-
-form.elements.name.addEventListener("input", () => {
-  form.elements.director.value = form.elements.name.value;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let ongoing = false;
+  if (form.elements.ongoing.value === "yes") {
+    ongoing = true;
+  }
+  const obj = {
+    genres: form.elements.genres.value.split("\n"),
+    seasons: form.elements.seasons.value,
+    director: form.elements.director.value,
+    rating: form.elements.rating.value,
+    ongoing: ongoing,
+    name: form.elements.name.value,
+  };
+  addShow(obj);
 });
-//.length
-
-/*
-if the input field has a certain length, then find the next input field and give it focus
-
-DRY - Don't Repeat Yourself
-WET - Write everything twice
-AHA - Avoid Hasty Abstractions
-
-First make it work, then make it right
-*/
