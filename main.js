05f24683da95710fa36de2e1bce118cf185d5527
Jonas Holbech <jh@jonasholbech.dk>
2022-10-27 15:29:47 +0200

    credit stuff

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ form.elements.name.addEventListener("input", () => {
   form.elements.director.value = form.elements.name.value;
 });
 //.length
+
+/*
+if the input field has a certain length, then find the next input field and give it focus
+
+DRY - Don't Repeat Yourself
+WET - Write everything twice
+AHA - Avoid Hasty Abstractions
+
+First make it work, then make it right
+*/
